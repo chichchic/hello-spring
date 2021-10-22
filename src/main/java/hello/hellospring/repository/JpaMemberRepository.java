511ca2769defe8b5933bb2e43b1234860b7ae7db
chicichich <pch6789@naver.com>
2021-10-22 16:54:36 +0900

    feat: Connect DB with Spring Data JPA

--- src/main/java/hello/hellospring/repository/JpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -7,7 +7,7 @@ import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+//@Repository
 public class JpaMemberRepository implements MemberRepository {
 
     private final EntityManager entityManager;
